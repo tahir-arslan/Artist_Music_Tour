409f0ac3a6ffe3d0f919c0a456c2d4dbc16a4920
Kevin Reid <kevinnivek@me.com>
2022-06-14 12:27:22 -0400

    trying Api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 var BandsInTownAPIKey = "5d493638-f008-4575-8af2-f799806c784e"
 var TicketMasterAPIKey = "9nXT8eGYX3feuBixAjH6OjuUYJOitJlo"
 
-// var getArtistInformation = function () {
-//  var apiURL = "https://rest.bandsintown.com/artists/Grace%20Jones?app_id=5d493638-f008-4575-8af2-f799806c784e";
-// //  var apiURL = "https://app.ticketmaster.com/discovery/v1/events.json?apikey=" + TicketMasterAPIKey;
+var getArtistInformation = function () {
+ var apiURL = "https://rest.bandsintown.com/artists/Grace%20Jones?app_id=" + BandsInTownAPIKey;
+//  var apiURL = "https://app.ticketmaster.com/discovery/v1/events.json?apikey=" + TicketMasterAPIKey;
    
 
-//  // fethcing data and JSON the data
-//  fetch(apiURL).then(function(response) {
-//     console.log(response);
-//  });
-// }
+ // fetching data and JSON the data
+ fetch(apiURL).then(function(response) {
+    console.log(response);
+ });
+}
 
-// getArtistInformation();
+getArtistInformation();
 
 
-$.getJSON("http://api.bandsintown.com/artists/weezer/events.json?callback=?&app_id=" + BandsInTownAPIKey , function(result) {
+// $.getJSON("http://api.bandsintown.com/artists/weezer/events.json?callback=?&app_id=" + BandsInTownAPIKey , function(result) {
 
-    $.each(result, function(key, val) {
-      alert(key + val);
-    });
-});
+//     $.each(result, function(key, val) {
+//       alert(key + val);
+//     });
+// });

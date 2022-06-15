43cc932c979e019b1e18b388fbb1db4b5e1b442d
Leoman <leo.pinzon102@gmail.com>
2022-06-15 18:32:41 -0400

    adding myFuntion to limit the API KEY quote during testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var videoContainer = document.querySelector(".video-container");
 var apiLink = "https://www.googleapis.com/youtube/v3/search?"
 var youtubeKey = "AIzaSyAVipUFCUajMgvasF6xv_p18pu4uLXmhcE"
 var youtubeUrl = 'https://www.youtube.com/watch?v='
-var search = "Nirvana"
+var search = "The offspring"
 console.log(apiLink)
 var url= `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${youtubeKey}&q=${search}&maxResults=3`;
 
@@ -18,26 +18,22 @@ var myFunction = function(){
                 for(video of videos){
                     videoContainer.innerHTML += `
                     <div class="video-item">
-                        <a href="${youtubeUrl + video.id.videoId }"> 
+                        <a href="${youtubeUrl + video.id.videoId}" class="video-link" target="_blank"> 
                         ${video.snippet.title}</a>
-                        <img src="${video.snippet.thumbnails.default.url}">
+                        <img class="video-img" src="${video.snippet.thumbnails.default.url}" />
                     </div>
                     `
-                    console.log(video.snippet.title)
-                    console.log(video.snippet.thumbnails.default.url)
-                    console.log( youtubeUrl + video.id.videoId )
+                    // console.log(video.snippet.title)
+                    // console.log(video.snippet.thumbnails.default.url)
+                    // console.log( youtubeUrl + video.id.videoId )
 
                 }
-                //'https://www.youtube.com/watch?v='
             })
             
         }
         else{
             console.log("error")
         }
-            // response.json().then(function(data){
-            //     console.log(data)
-            // })
     })
 }
 // myFunction()

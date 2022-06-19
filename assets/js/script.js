b05d7df882dc93a22e0ccd34d5911957a8f307ce
kevin knight <ksknight81@gmail.com>
2022-06-19 15:37:34 -0400

    updated js to show only the valid history

--- assets/js/script.js
+++ assets/js/script.js
@@ -237,32 +237,36 @@ var getArtistName = function() {
     // event listener to get artist name input by user in the input text box
     artistNameInput.addEventListener("change", getArtistName);
 
-
-    console.log();
-
     // search history - pulls from local storage using the 'search' button and if it has no value, creates a new blank array
     var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
+    var search_results = document.querySelector("#past-searches");
+    var searchHistoryEl = document.createElement("li");
+
     // function to save local storage data
     var saveArtist = function(ArtistNameInput) {
+        
         //this pushes the value of ... to the searchHistory array
         searchHistory.push(ArtistNameInput);
         // verify if it shows the value of searchHistory
         console.log(searchHistory);
         // this sets the value of searchHistory to be filled in by the JSON
         localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-        loadPreviousArtist();
+
+        // append new artist to the bottom of the list
+        searchHistoryEl.innerHTML = ArtistNameInput;
+        search_results.appendChild(searchHistoryEl);        
     }
 
     // function to load local storage data
     var loadPreviousArtist = function () {
+        searchHistoryEl = "";
+        // debugger;
         // function to pull / create the list items on the page if there is data
         for (i = 0; i < searchHistory.length; i++) {
             // variables for search results
-            searchHistoryEl = "";
             var search_results = document.querySelector("#past-searches");
             var searchHistoryEl = document.createElement("li");
-           
             // when clicking on the searchHistoryEl (values from previous), the function reSearch is actioned
             searchHistoryEl.onclick = reSearch;
             // setting the value on past-searches
@@ -270,13 +274,13 @@ var getArtistName = function() {
             // populating the values in the html
             searchHistoryEl.innerHTML = searchHistory[i];
             search_results.appendChild(searchHistoryEl); 
-        }
+            }
     }
-
+//location.reload();
     // function to search by previous search
     function reSearch(event) {
         // prevent from refreshing
-        event.preventDefault();
+       // event.preventDefault();
         // get value from input element 
         var setArtistName = event.target.id;
         
